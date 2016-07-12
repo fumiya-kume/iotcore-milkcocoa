967fc09330adb5dd705cd4064a09381721054275
久米　史也 <fumiya.kume@hotmail.com>
2016-07-13 01:31:39 +0900

    Add Pi

--- Remote-Temp-client/Remote-Temp-Host/MainPage.xaml.cs
+++ Remote-Temp-client/Remote-Temp-Host/MainPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using HDC1000;
 using Milkcocoa;
 using Newtonsoft.Json;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
+using static Newtonsoft.Json.JsonConvert;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
 
@@ -28,19 +33,28 @@ namespace Remote_Temp_Host
             HDC1000Data resultobject = JsonConvert.DeserializeObject<HDC1000Data>(result[0].ToString());
             Temp_Value.Text = $"Temp: {(int)resultobject.Temp}°";
             Hum_Value.Text = $"Hum: {(int)resultobject.Hum}%";
+            GraphChart.Title = "過去のデータ";
+            (GraphChart.Series as LineSeries).ItemsSource = await GetDataList();
+        }
+
+        private async Task<IEnumerator<dynamic>> GetDataList()
+        {
+            var result = await DataStore.history().run();
+            return  result.Select(o => DeserializeObjectAsync<HDC1000Data>(o.ToString())).GetEnumerator();
+
         }
 
         private void singleViewbutton_Click(object sender, RoutedEventArgs e)
         {
-            this.Temp_Value.Visibility = Visibility.Visible;
-            this.Hum_Value.Visibility = Visibility.Visible;
+            SingleViewHost.Visibility = Visibility.Visible;
+            GraphViewHost.Visibility = Visibility.Collapsed;
             this.Humbagerview.IsPaneOpen = false;
         }
 
         private void GraphViewbutton_Click(object sender, RoutedEventArgs e)
         {
-            this.Temp_Value.Visibility = Visibility.Collapsed;
-            this.Hum_Value.Visibility = Visibility.Collapsed;
+            SingleViewHost.Visibility = Visibility.Collapsed;
+            GraphViewHost.Visibility = Visibility.Visible;
             this.Humbagerview.IsPaneOpen = false;
         }
     }
